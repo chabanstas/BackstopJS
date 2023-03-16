5b0ec60e9f7706cb1cf164d623cc32f46fbcc847
chabanstas <105775681+chabanstas@users.noreply.github.com>
2023-03-16 23:12:09 +0000

    Build: (b2bbd7d) Update backstopjs.yml

---
+++ html_report/config.js
@@ -0,0 +1,30 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
+        "test": "../bitmaps_test/20230316-231202/backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
+        "selector": "document",
+        "fileName": "backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
+        "label": "Test WebdriverIO v5 and v6 pages",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://v5.webdriver.io/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "iPhone8",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
