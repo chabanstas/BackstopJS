e8ec5fee018e470316a6880070c9c377893aea18
chabanstas <105775681+chabanstas@users.noreply.github.com>
2023-03-17 14:35:32 +0000

    Build: (b789f6a) Update README.md

--- html_report/config.js
+++ html_report/config.js
@@ -4,7 +4,7 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
-        "test": "../bitmaps_test/20230317-142449/backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
+        "test": "../bitmaps_test/20230317-143525/backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
         "selector": "document",
         "fileName": "backstop_default_Test_WebdriverIO_v5_and_v6_pages_0_document_0_iPhone8.png",
         "label": "Test WebdriverIO v5 and v6 pages",
